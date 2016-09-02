2016c2aa7a76f0207de3ddd350696e3f29a1d816
Nandhakumar S <nandhakumaryogesh@gmail.com>
2016-09-02 21:43:15 +0530

    Delete Solution.java

--- Solution.java
+++
@@ -1,9 +0,0 @@
-package Org.Creators.MovieBuff;
-
-public class Solution {
-	public static void main(String args[]){
-		//HashMap<String, MovieBuff> hm = new HashMap<String, MovieBuff>();
-	    MovieMenu mm = new MovieMenu();  
-		mm.menu();
-	}
-}
